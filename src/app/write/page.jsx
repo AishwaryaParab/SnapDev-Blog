c40b8f04671eee1673f7220917c4aa39460b0179
Aishwarya Parab <aiparab@ebay.com>
2023-10-23 20:15:25 +0530

    Added toast messages for uploading images and publishing posts

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import styles from "./write.module.css";
 import Image from 'next/image';
-import ReactQuill from 'react-quill';
+// import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.core.css'
 import "react-quill/dist/quill.bubble.css";
 import { useSession } from 'next-auth/react';
@@ -14,6 +14,10 @@ import hljs from 'highlight.js'
 import 'highlight.js/styles/atom-one-dark.css'
 import Select from 'react-select';
 import { getApiUrl } from '@/utils/apiUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ThemeContext } from '@/context/ThemeContext';
+import dynamic from 'next/dynamic';
 
 // Initialize highlight.js for code syntax highlighting
 hljs.configure({
@@ -53,6 +57,7 @@ const options = [
 const WritePage = () => {
   const {status} = useSession();
   const router = useRouter();
+  const ReactQuill = dynamic(() => import("react-quill"), { srr: false });
 
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
@@ -62,6 +67,9 @@ const WritePage = () => {
   const [catSlug, setCatSlug] = useState(null);
 
   const storage = getStorage(app);
+
+  const {theme} = useContext(ThemeContext);
+  const toastId = useRef(null);
 
   // Aishwarya Parab -> aishwarya-parab
   const slugify = (str) => {
@@ -87,7 +95,21 @@ const WritePage = () => {
       })
     });
 
-    // console.log(res);
+    if(res.status == 200) {
+      toast.success('Your post has been published!', {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: theme === "light" ? "dark" : "light",
+      });
+      setTimeout(() => {
+        router.push("/");
+      }, 2000)
+    }
   }
 
   const handleSelectChange = ( selectedOption ) => {
@@ -101,44 +123,56 @@ const WritePage = () => {
       router.push("/");
     }
 
-    if(typeof window !== "undefined") { // this is a client side operation
-        const upload = () => {
-        const fileName = new Date().getTime() + file.name; // for a unique file name
-        const storageRef = ref(storage, fileName);
-    
-        const uploadTask = uploadBytesResumable(storageRef, file);
-    
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {
-            const progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            // console.log("Upload is " + progress + "% done");
-            // switch (snapshot.state) {
-            //   case "paused":
-            //     console.log("Upload is paused");
-            //     break;
-            //   case "running":
-            //     console.log("Upload is running");
-            //     break;
-            // }
-          },
-          (error) => {
-            // Handle unsuccessful uploads
-          },
-          () => {
-            // Handle successful uploads on complete
-            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              // console.log("File available at", downloadURL);
-              setMedia(downloadURL);
+    const upload = () => {
+      const fileName = new Date().getTime() + file.name; // for a unique file name
+      const storageRef = ref(storage, fileName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          // check if we already displayed a toast
+          if (toastId.current === null) {
+            toastId.current = toast("Upload in Progress", {
+              progress,
+              theme: theme === "light" ? "dark" : "light",
+            });
+          } else {
+            toast.update(toastId.current, {
+              progress,
+              theme: theme === "light" ? "dark" : "light",
             });
           }
-        );
-      };
+          console.log("Upload is " + progress + "% done");
+          // switch (snapshot.state) {
+          //   case "paused":
+          //     console.log("Upload is paused");
+          //     break;
+          //   case "running":
+          //     console.log("Upload is running");
+          //     break;
+          // }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            // console.log("File available at", downloadURL);
+            toast.done(toastId.current);
+            setMedia(downloadURL);
+          });
+        }
+      );
+    };
   
-      file && upload();
-    }
+    file && upload();
   }, [file, status])
 
   if(status == "loading") {
@@ -191,10 +225,13 @@ const WritePage = () => {
           </label>
         )}
 
+        {/* Ensure ReactQuill is only rendered on the client side */}
         <ReactQuill className={styles.textArea} theme="bubble" modules={toolbarOptions} value={value} onChange={setValue} placeholder="Tell your story..." />
       </div>
 
       <button className={styles.publish} onClick={handleSubmit}>Publish</button>
+
+      <ToastContainer />
     </div>
   )
 }
